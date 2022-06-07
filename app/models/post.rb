1a71a25e4b199dcd15fb45eeb93ea05162610795
Samantha Crespo <samkcrespo@gmail.com>
2022-06-07 16:17:49 -0700

    commit tree 4e5d1e26d0c37fdcaab98ef4d850dc399acb2b65

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+
+  validate :clickbait
+
+  CLICKBAIT_WORDS = [/Won't Believe/i, /Secret/i, /Top\d/i, /Guess/i]
+
+  def clickbait
+    if CLICKBAIT_WORDS.none? do |bait|
+         bait.match title
+       end
+      errors.add(:title, "Must be clickbait")
+    end
+  end
+
 end
